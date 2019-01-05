6d26c326d575527bb934acfbbb73b24435ba41a0
Lawrence <lawrence@ITs-MacBook-Pro.local>
2019-01-05 01:36:01 -0600

    finalized project

--- quake_app/lib/main.dart
+++ quake_app/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+import 'package:intl/intl.dart';
+
+// Date format: https://docs.flutter.io/flutter/intl/DateFormat-class.html
 
 Map _data;
 List _features;
@@ -36,29 +39,62 @@ class Quakes extends StatelessWidget {
               // crafting rows and list views
               if (position.isOdd) return Divider();
               final index = position ~/ 2; // dividing by 2 and returning int.
+              var format = DateFormat.yMMMMd("en_US").add_jm();
+              //var dateString = format.format(date);
+
+              var date = format.format(DateTime.fromMillisecondsSinceEpoch(
+                  _features[position]['properties']['time'],
+                  isUtc: true));
+
               return ListTile(
                 title: Text(
-                  "Mag: ${_features[position]['properties']['mag']}",
+                  "At: $date",
                   style: TextStyle(
                       fontSize: 19.5,
                       color: Colors.orange,
                       fontWeight: FontWeight.w500),
                 ),
-                subtitle: Text("${_features[position]['properties']['place']}",
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14.5,
-                  color: Colors.grey,
-                  fontStyle: FontStyle.italic,
-                ),),
+                subtitle: Text(
+                  "${_features[position]['properties']['place']}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14.5,
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
                 leading: CircleAvatar(
                   backgroundColor: Colors.greenAccent,
-                  child: Text("${_features[position]['properties']['mag']}"),
+                  child: Text(
+                    "${_features[position]['properties']['mag']}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.5,
+                      color: Colors.red,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
                 ),
+                onTap: () {
+                  _showAlertMessage(context, "Location: ${_features[index]['properties']['title']}");
+                },
               );
             }),
       ),
     );
+  }
+
+  void _showAlertMessage(BuildContext context, String message) {
+    var alert = AlertDialog(
+      title: Text('Quakes'),
+      content: Text(message),
+      actions: <Widget>[
+        FlatButton(onPressed: (){Navigator.pop(context);},
+            child: Text('OK'))
+      ],
+
+    );
+    showDialog(context: context, builder: (context)=>alert);
   }
 }
 
