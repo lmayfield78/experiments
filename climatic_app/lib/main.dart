75a01a03b03bb5e2c171186656d25b25576e1eb0
Lawrence <lawrence@ITs-MacBook-Pro.local>
2019-01-05 03:45:05 -0600

    update climate project

---
+++ climatic_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './ui/climatic.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Climatic App',
+    home: Climatic()
+  ));
+}
+
---
+++ climatic_app/lib/ui/climatic.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class Climatic extends StatefulWidget {
+  @override
+  _ClimaticState createState() => _ClimaticState();
+}
+
+class _ClimaticState extends State<Climatic> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Climatic'),
+        backgroundColor: Colors.blueGrey,
+        actions: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
+
