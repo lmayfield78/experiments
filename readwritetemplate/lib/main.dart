07034b2080f6e61bc6a0edd1d86a463829ec5a55
spsonline1 <lawrencemayfield@hotmail.com>
2019-01-08 19:11:23 -0600

    Update

--- readwritetemplate/lib/main.dart
+++ readwritetemplate/lib/main.dart
@@ -21,18 +21,27 @@ to write data to a file and to read that data.
  */
 
 class _HomeState extends State<Home> {
+  var _enterDataField = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Read / Write'),
+        centerTitle: true,
+        backgroundColor: Colors.purpleAccent,
+      ),
+      body: Container(
 
+          ),
+    );
+  }
 
   // This method locates and returns the directory
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path; // Home Directory
   }
-
 
   // This method will create the file in its directory
   Future<File> get _localFile async {
@@ -50,18 +59,15 @@ class _HomeState extends State<Home> {
   }
 
   // This method will read the local file and store it in a variable
-  Future<String> readData() async{
-
+  Future<String> readData() async {
     try {
       final file = await _localFile;
 
       String data = await file.readAsString();
 
       return data;
-
-    }catch (e){
-        return "Nothing saved!";
+    } catch (e) {
+      return "Nothing saved!";
     }
   }
-
 }
