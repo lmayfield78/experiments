2326fdfd34b670cc78dbbf7673146edbd3a912dc
spsonline1 <lawrencemayfield@hotmail.com>
2019-01-14 17:23:17 -0600

    finished firebase template

--- firebase_basic_template/lib/main.dart
+++ firebase_basic_template/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_basic_template/model/board.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_database/firebase_database.dart';
-
-
-final FirebaseDatabase database = FirebaseDatabase.instance;
 
 void main() => runApp(MyApp());
 
@@ -14,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Community Board',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -23,19 +20,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  List<Board> boardMessages = List();
-  Board board;
-
-
-
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Board> boardMessages = List();
+  Board board;
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  DatabaseReference databaseReference;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    board = Board("", "");
+    databaseReference = database.reference().child("community_board");
+    databaseReference.onChildAdded.listen(_onEntryAdded);
+    databaseReference.onChildChanged.listen(_onEntryChanged);
+  }
 //  void _incrementCounter() {
 //
 //    database.reference().child("message").set(
@@ -56,23 +60,86 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text("text"),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
+        appBar: AppBar(
+          title: Text("text"),
         ),
-      ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 0,
+              child: Center(
+                  child: Form(
+                      key: formKey,
+                      child: Flex(
+                        direction: Axis.vertical,
+                        children: <Widget>[
+                          ListTile(
+                            leading: Icon(Icons.subject),
+                            title: TextFormField(
+                              initialValue: "",
+                              onSaved: (val) => board.subject = val,
+                              validator: (val) => val == "" ? val : null,
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.message),
+                            title: TextFormField(
+                              initialValue: "",
+                              onSaved: (val) => board.body = val,
+                              validator: (val) => val == "" ? val : null,
+                            ),
+                          ),
+                          FlatButton(
+                              color: Colors.redAccent,
+                              onPressed: () {
+                                handleSubmit();
+                              },
+                              child: Text("post"))
+                        ],
+                      ))),
+            ),
+            Flexible(
+                child: FirebaseAnimatedList(
+                    query: databaseReference,
+                    itemBuilder: (_, DataSnapshot snapshot,
+                        Animation<double> animation, int index) {
+                      return Card(
+                        child: ListTile(
+                          leading: CircleAvatar(backgroundColor: Colors.red),
+                          title: Text(boardMessages[index].subject),
+                          subtitle: Text(boardMessages[index].body),
+                        ),
+                      );
+                    }))
+          ],
+        ));
+  }
 
-    );
+  void _onEntryAdded(Event event) {
+    setState(() {
+      boardMessages.add(Board.fromSnapshot(event.snapshot));
+    });
+  }
+
+  void handleSubmit() {
+    final FormState form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      form.reset();
+      // Save form data to database
+      databaseReference.push().set(board.toJson());
+    }
+  }
+
+  void _onEntryChanged(Event event) {
+    var oldEntry = boardMessages.singleWhere((entry){
+      return entry.key == event.snapshot.key;
+    });
+
+    setState(() {
+      boardMessages[boardMessages.indexOf(oldEntry)] = Board.fromSnapshot(event.snapshot);
+    });
+
   }
 }
