7686fa999dc3f19081e17012cbfd1fa40ab7212e
Lawrence <lawrence@ITs-MacBook-Pro.local>
2019-01-15 11:47:33 -0600

    Added signin

--- firebase_basic_template/lib/main.dart
+++ firebase_basic_template/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_basic_template/model/board.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MyApp());
 
---
+++ firebase_signin_template/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:firebase_basic_template/model/board.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Community Board',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Board> boardMessages = List();
+  Board board;
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  DatabaseReference databaseReference;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    board = Board("", "");
+    databaseReference = database.reference().child("community_board");
+    databaseReference.onChildAdded.listen(_onEntryAdded);
+    databaseReference.onChildChanged.listen(_onEntryChanged);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Board"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                    child: Text("Google Sign In"),
+                    onPressed: () => _googleSignin(),
+                    color: Colors.red),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text("Sign in with Email"),
+                  color: Colors.green,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text("Create Account"),
+                  color: Colors.lightBlueAccent,
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  _googleSignin() {}
+
+  void _onEntryAdded(Event event) {
+    setState(() {
+      boardMessages.add(Board.fromSnapshot(event.snapshot));
+    });
+  }
+
+  void handleSubmit() {
+    final FormState form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      form.reset();
+      // Save form data to database
+      databaseReference.push().set(board.toJson());
+    }
+  }
+
+  void _onEntryChanged(Event event) {
+    var oldEntry = boardMessages.singleWhere((entry) {
+      return entry.key == event.snapshot.key;
+    });
+
+    setState(() {
+      boardMessages[boardMessages.indexOf(oldEntry)] =
+          Board.fromSnapshot(event.snapshot);
+    });
+  }
+}
