63ed111110bc094952368fdc09ed88f239757313
Lawrence <lawrence@ITs-MacBook-Pro.local>
2019-02-13 19:12:40 -0600

    Added weather app 2

---
+++ weatherapp_2/lib/ui/myhomepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp_2/ui/weather.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+
+          title: Text(widget.title),
+        ),
+        body: Weather()
+    );
+  }
+}
---
+++ weatherapp_2/lib/ui/weather.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+
+class Weather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(60.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+      ),
+    );
+  }
+}
